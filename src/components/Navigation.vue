97ee65cc3dc0fab66fab705974c9a037b7a4c311
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-03-11 23:01:47 +0300

    Create Navigation.vue

---
+++ src/components/Navigation.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <nav class="fixed bottom-0 left-0 right-0 bg-gray-900 border-t border-gray-800">
+    <div class="flex justify-around items-center h-16">
+      <button
+        v-for="tab in tabs"
+        :key="tab.id"
+        @click="$emit('change-tab', tab.id)"
+        class="flex flex-col items-center justify-center w-full h-full"
+        :class="activeTab === tab.id ? 'text-blue-400' : 'text-gray-500'"
+      >
+        <component :is="tab.icon" size="24" />
+        <span class="text-xs mt-1">{{ tab.label }}</span>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { Home, Wallet, ListTodo, Users } from 'lucide-vue-next'
+
+export default {
+  name: 'Navigation',
+  props: {
+    activeTab: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['change-tab'],
+  setup() {
+    const tabs = [
+      { id: 'home', icon: Home, label: 'الرئيسية' },
+      { id: 'tasks', icon: ListTodo, label: 'المهام' },
+      { id: 'referrals', icon: Users, label: 'الإحالات' },
+      { id: 'wallet', icon: Wallet, label: 'المحفظة' }
+    ]
+
+    return {
+      tabs
+    }
+  }
+}
+</script>
