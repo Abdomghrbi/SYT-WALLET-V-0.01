9aae4e61857ee802832bb79a5bfa1c56317258b1
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-03-23 16:27:00 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/components/**/*.{vue,js}",
+    "./src/views/**/*.{vue,js}",
+    "./src/**/*.vue"
   ],
   theme: {
     extend: {
