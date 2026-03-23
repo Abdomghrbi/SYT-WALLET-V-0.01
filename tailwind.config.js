48c6c2af6014bc06513e7fa70e3cab69ebaf34a0
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-03-23 22:57:04 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./src/components/**/*.{vue,js}",
-    "./src/views/**/*.{vue,js}",
-    "./src/**/*.vue"
   ],
   theme: {
-    extend: {
-      colors: {
-        tg: {
-          bg: 'var(--tg-theme-bg-color)',
-          text: 'var(--tg-theme-text-color)',
-          hint: 'var(--tg-theme-hint-color)',
-          link: 'var(--tg-theme-link-color)',
-          button: 'var(--tg-theme-button-color)',
-          buttonText: 'var(--tg-theme-button-text-color)',
-          secondaryBg: 'var(--tg-theme-secondary-bg-color)',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
